f65f3abc3e3b9156239dd7830c37221352f0c530
Thomas Korner <3245718+tkorner@users.noreply.github.com>
2025-09-02 17:59:25 +0200

    added capability

--- db/access.php
+++ db/access.php
@@ -1,5 +1,29 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// @package    local_cohortunenroller
+// @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
 defined('MOODLE_INTERNAL') || die();
 
-// Keine eigenen Capabilities nötig, wir prüfen auf moodle/cohort:assign.
-$capabilities = [];
+/**
+ * Capabilities for local_cohortunenroller plugin.
+ *
+ * Defines the custom capability 'local/cohortunenroller:run'.
+ * This allows controlling which roles are permitted to run the plugin.
+ * By default, the Manager role is granted this capability.
+ */
+$capabilities = [
+    'local/cohortunenroller:run' => [
+        'riskbitmask'  => RISK_XSS,
+        'captype'      => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes'   => [
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
--- lang/en/local_cohortunenroller.php
+++ lang/en/local_cohortunenroller.php
@@ -1,5 +1,17 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// @package    local_cohortunenroller
+// @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+defined('MOODLE_INTERNAL') || die();
+
 $string['pluginname'] = 'Cohort Unenroller';
+$string['cohortunenroller:run'] = 'Run the Cohort Unenroller';
 $string['pageheading'] = 'Cohort Unenroller';
 $string['uploadcsv'] = 'Upload CSV';
 $string['csvhelp'] = 'CSV headers: username,cohortid OR username,cohortidnumber.';
