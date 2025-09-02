d176d6b03a3f9f89c17304f3dd11df9299941132
Thomas Korner <3245718+tkorner@users.noreply.github.com>
2025-09-02 15:48:04 +0200

    Moodle Plugin CI tests integrated
    Co-Authored-By: Luca Bösch <377279+lucaboesch@users.noreply.github.com>

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_cohortunenroller';
-$plugin->version   = 2025090104;   // YYYYMMDDXX
+$plugin->version   = 2025090201;   // YYYYMMDDXX
 $plugin->requires  = 2024052000;   // Moodle 4.5
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.3.2';
+$plugin->release   = '0.4';
