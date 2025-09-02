839e6f7717199068ed949e62005590e813ed23fb
Thomas Korner <3245718+tkorner@users.noreply.github.com>
2025-09-02 18:03:19 +0200

    version update

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_cohortunenroller';
-$plugin->version   = 2025090201;   // YYYYMMDDXX
+$plugin->version   = 2025090202;   // YYYYMMDDXX
 $plugin->requires  = 2024052000;   // Moodle 4.5
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.4';
+$plugin->release   = '0.4.1';
